e4204e7ed20ee7a9cea2382a21715f46711a850a
IgorHamzi <igorwhamzi@gmail.com>
2021-08-26 19:17:15 -0300

    adiciona task 1

---
+++ fundamentos/bloco4-introducao-a-javascript-e-logica-de-programacao/dia-1/task1.js
@@ -0,0 +1,8 @@
+const a = 2;
+const b = 4;
+
+console.log("adição = " + (a + b));
+console.log("subtração = " + (a - b));
+console.log("multiplicação = " + (a * b));
+console.log("divisão = " + (a / b));
+console.log("resto = " + (a % b));
